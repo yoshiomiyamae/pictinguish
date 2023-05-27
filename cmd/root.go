97ec4ae04ae586eee5bbd1b4755af196a5c57f90
Yoshio Miyamae <yoshio.miyamae@famoce-succellion.com>
2023-05-27 07:42:47 +0000

    first commit

---
+++ cmd/root.go
@@ -0,0 +1,106 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/rwcarlsen/goexif/exif"
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "pictinguish",
+	Short: "",
+	Long:  ``,
+	Args:  cobra.MinimumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		dest, err := cmd.Flags().GetString("dest")
+		src := args[0]
+		if err != nil {
+			return err
+		}
+
+		if dest == "" {
+			dest = filepath.Dir(src)
+		}
+
+		fileInfoList, err := ioutil.ReadDir(src)
+		if err != nil {
+			return err
+		}
+
+		for _, fileInfo := range fileInfoList {
+			if fileInfo.IsDir() {
+				continue
+			}
+
+			srcFileName := fileInfo.Name()
+			srcFilePath := filepath.Join(src, srcFileName)
+
+			fmt.Println(srcFileName)
+
+			file, err := os.Open(srcFilePath)
+			if err != nil {
+				fmt.Println("=> Couldn't open", err)
+				continue
+			}
+
+			switch filepath.Ext(srcFileName) {
+			case ".jpg", ".jpeg", ".png", ".gif", ".tiff", ".tif":
+				{
+					x, err := exif.Decode(file)
+					if err != nil {
+						fmt.Println("=> Couldn't decode exif", err)
+						continue
+					}
+
+					date, err := x.DateTime()
+					if err != nil {
+						fmt.Println("=> Couldn't get date", err)
+						continue
+					}
+					destDirPath := filepath.Join(dest, date.Format("2006-01-02_15-04-05"))
+					destDirInfo, err := os.Stat(destDirPath)
+					if err != nil || !destDirInfo.IsDir() {
+						err = os.Mkdir(destDirPath, 0755)
+						if err != nil {
+							fmt.Println(err)
+						}
+					}
+					destFilePath := filepath.Join(
+						destDirPath,
+						srcFileName,
+					)
+					os.Rename(
+						srcFilePath,
+						destFilePath,
+					)
+					fmt.Println("=>", destFilePath)
+					continue
+				}
+			}
+			fmt.Println("=> Skip")
+		}
+
+		return nil
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().StringP("dest", "d", "", "Destination")
+}
---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/yoshiomiyamae/pictinguish
+
+go 1.20
+
+require (
+	github.com/dsoprea/go-logging v0.0.0-20200710184922-b02d349568dd // indirect
+	github.com/dsoprea/go-utility/v2 v2.0.0-20221003172846-a3e1774ef349 // indirect
+	github.com/go-errors/errors v1.4.2 // indirect
+	github.com/golang/geo v0.0.0-20210211234256-740aa86cb551 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/rwcarlsen/goexif v0.0.0-20190401172101-9e8deecbddbd // indirect
+	github.com/spf13/cobra v1.7.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/net v0.0.0-20221002022538-bcab6841153b // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
